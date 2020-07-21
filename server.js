692891ba749a8f9f30892cd393cb441a8e147e0c
myusername <Matanfriedleet@hotmail.com>
2020-07-21 13:08:50 +0300

    added heroku config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require('./server/routes/api');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 
-mongoose.connect('mongodb://localhost/weather', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weather', { useNewUrlParser: true });
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(bodyParser.json())
@@ -13,5 +13,5 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', api);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Running server on port ${ port }`));
